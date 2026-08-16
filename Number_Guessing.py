54bdf0e5a065a70db3dda040724df3153494e88e
ADITYA PATIL <23uet081@dkte.ac.in>
2026-08-16 23:05:42 +0530

    Update Number_Guessing.py

--- Number_Guessing.py
+++ Number_Guessing.py
@@ -26,21 +26,25 @@ guess = st.number_input(
     max_value=100,
     step=1
 )
+if st.session_state.attempts <= 9:
+    if st.button("Guess"):
+        if not st.session_state.game_over:
+            st.session_state.attempts += 1
 
-if st.button("Guess"):
-    if not st.session_state.game_over:
-        st.session_state.attempts += 1
-
-        if guess < st.session_state.number:
-            st.warning("📉 Too Small!")
-        elif guess > st.session_state.number:
-            st.warning("📈 Too Large!")
-        else:
-            st.success(
-                f"🎉 Congratulations! You guessed the number in {st.session_state.attempts} attempts."
-            )
-            st.balloons()
-            st.session_state.game_over = True
+            if guess < st.session_state.number:
+                st.warning("📉 Too Small!")
+            elif guess > st.session_state.number:
+                st.warning("📈 Too Large!")
+            else:
+                st.success(
+                    f"🎉 Congratulations! You guessed the number in {st.session_state.attempts} attempts."
+                )
+                st.balloons()
+                st.session_state.game_over = True
+else:
+    st.error(
+        "You lost⛔."
+    )
 
 if st.button("New Game"):
     st.session_state.number = random.randint(1, 100)
